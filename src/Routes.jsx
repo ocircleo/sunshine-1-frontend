e38ffb072269e0f5eacc18e3d519418d8fc2b0b1
vacoule <salmanhossain11222626@hotmail.com>
2023-07-23 20:14:28 +0600

    login added

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home";
 import Errorpge from "./Errorpge";
 import Contact from "./pages/contact/Contact";
 import About from "./pages/About/About";
+import Login from "./pages/shared/login/Login";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,9 @@ const router = createBrowserRouter([
       }, {
         path: 'about',
         element: <About></About>
+      }, {
+        path: 'login',
+        element: <Login></Login>
       }
     ],
   },
--- src/pages/shared/navbar/Navbar.jsx
+++ src/pages/shared/navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             <div className="navbar-end">
                 <button className="p-3 bg-blue-500 rounded me-4 active:scale-90 duration-200" onClick={() => setLogin(!login)}>Acount</button>
                 <div className={`flex justify-stretch items-center gap-2  ${!login ? 'top-[3.5rem] invisible pointer-events-auto' : 'top-[4.5rem] visible pointer-events-auto'} w-[10rem] rounded right-4 absolute bg-white border flex-col capitalize duration-100 ease-in`}>
-                    <Link className="py-2 text-center hover:bg-gray-200  rounded w-full">login</Link>
+                    <Link to={'/login'} className="py-2 text-center hover:bg-gray-200  rounded w-full">login</Link>
                     <Link className="py-2 hover:bg-gray-200 text-center w-full rounded">signup</Link>
 
                 </div>
