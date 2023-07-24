7a5dff83bde15424e07182ff1bcf364c477c79fd
vacoule <salmanhossain11222626@hotmail.com>
2023-07-24 21:59:04 +0600

    about and contact page update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,45 @@
 
 const About = () => {
     return (
-        <div>
-            This is about
+        <div className="container mx-auto">
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">About us</h2>
+
+R Josb Tech Jobs is a leading technology-focused recruitment company that specializes in connecting highly skilled professionals with diverse job opportunities in the tech industry. Our mission is to bridge the gap between exceptional talent and innovative companies, ensuring a perfect match that drives growth and success.
+<br></br>
+**Our Vision**
+<br></br>
+At R Josb, we envision a future where every tech professional finds their dream job, and every tech company finds their perfect team member. We strive to be the go-to platform for both job seekers and employers in the tech sector, creating a seamless and efficient hiring process that benefits all parties involved.
+<br></br>
+**Our Services**
+<br></br>
+1. **Tech Job Placement:** We curate a wide range of tech job openings from startups to established tech giants, covering diverse roles such as software developers, data scientists, UX/UI designers, product managers, cybersecurity experts, and more.
+
+2. **Talent Sourcing and Screening:** Our experienced team of recruiters rigorously assesses candidates to ensure they possess the necessary skills and experience required by our partner companies. We only present top-tier talent to clients, saving them time and resources.
+
+3. **Personalized Matching:** Understanding the unique needs of both employers and candidates, we provide personalized job matches that align with skillsets, cultural fit, and career aspirations, resulting in lasting and fulfilling job placements.
+
+4. **Tech Industry Insights:** R Josb stays up-to-date with the latest trends and developments in the tech industry. We offer valuable insights and advice to our clients, helping them make informed decisions in a fast-evolving market.
+
+5. **Candidate Support:** We provide guidance and support to job seekers throughout the entire hiring process, from resume optimization to interview preparation, empowering them to present themselves as the best candidates.
+
+6. **Client Collaboration:** Collaborating closely with our partner companies, we ensure a deep understanding of their requirements, corporate culture, and long-term goals to find candidates who not only meet the job requirements but also align with the company's values and vision.
+<br></br>
+**Why Choose R Josb Tech Jobs**
+<br></br>
+- **Extensive Network:** With an extensive network of tech professionals and industry-leading companies, we have access to a vast pool of talent and job opportunities.
+
+- **Expertise:** Our team of tech-savvy recruiters possesses in-depth knowledge of the tech industry, enabling them to identify the right candidates for specialized roles.
+
+- **Efficiency:** Through our efficient matching process, we reduce the time-to-hire and deliver quick results, ensuring that companies can meet their business objectives promptly.
+
+- **Personalized Approach:** We believe in quality over quantity. By taking a personalized approach to recruitment, we ensure that both candidates and companies find their perfect fit.
+<br></br>
+R Josb Tech Jobs is dedicated to driving innovation and progress in the tech industry by fostering successful partnerships between talented professionals and cutting-edge companies. Whether you are a tech enthusiast seeking your dream job or a tech company searching for top talent, R Josb Tech Jobs is your trusted partner in the journey to success.
+               
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,17 @@
 
 const Contact = () => {
     return (
-        <div>
-            This is contact page
-        </div>
+        <form className="flex flex-col container mx-auto px-1 gap-5 my-5">
+            <div className="flex flex-col md:flex-row gap-5 w-full">
+            <input type="text" placeholder="Your name" className="input input-bordered w-full  border-4" />
+            <input type="email" placeholder="Your Email" className="input input-bordered w-full border-4" />
+            </div>
+            <textarea className=" w-full border-4 rounded p-2" rows={8} placeholder="detaill"></textarea>
+            <div className=" flex ">
+
+            <button className="btn btn-primary">send</button>
+            </div>
+        </form>
     );
 };
 
